c6f6cecfe84e0e2e4cc85c20845e8fe9682d1e71
Elchin Aslanli <aslanlielchin@gmail.com>
2022-08-13 01:39:10 +0300

    hm

--- shortcut.bat
+++ shortcut.bat
@@ -1,8 +1,11 @@
 @echo off
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
+echo Dim strCurDir
+echo strCurDir    = oWS.CurrentDirectory
 echo sLinkFile = "%HOMEDRIVE%%HOMEPATH%\Desktop\bruh.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-echo oLink.TargetPath = "bruh.vbs" >> CreateShortcut.vbs
+
+echo oLink.TargetPath = strCurDir & "\bruh.vbs" , 0 >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
 cscript CreateShortcut.vbs
 del CreateShortcut.vbs
